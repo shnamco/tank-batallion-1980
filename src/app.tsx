a595f93f8edec514ffe5fe48f7f03924c0fd26fd
Andy B <andybarnov@gmail.com>
2021-03-21 21:41:18 +0100

    game page wip

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
 import { Login } from './pages/login/login';
+import { Game } from './pages/game/game';
 import './app.pcss';
 
 export const App = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <div className="container">
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route
-            exact
-            path="/"
-            render={() => {
-              return <Redirect to="/login" />;
-            }}
-          />
-        </Switch>
-      </div>
+      {/* @Andrei-Vorobei, I don't think we need container everywhere */}
+      {/* (see /game page) */}
+      {/* <div className="container"> */}
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/game" component={Game} />
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return <Redirect to="/login" />;
+          }}
+        />
+      </Switch>
+      {/* </div> */}
     </BrowserRouter>
   );
 };
