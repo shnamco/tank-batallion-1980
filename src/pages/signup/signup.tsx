b677d572261ca787e947169fd44e4609cf5b68a4
Kolya Evtikhovich <evklaus@gmail.com>
2021-03-23 22:45:52 +0300

    add arcade background

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from '@components/header/header';
 import { SignupForm } from './components/signup_form/signup_form';
 
 export const Signup: React.FC = () => {
   return (
-    <>
-      <Header />
+    <div className="arcade__background">
       <SignupForm />
-    </>
+    </div>
   );
 };
