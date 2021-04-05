2d45abeab906e0ae77cc7bdfc4969e07bac209a1
Andrei Marchenko <marchenko_ad@mail.ru>
2021-04-05 12:06:50 +0300

    first commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundry } from '@components/error_boundary/error_boundary';
 import { Pages } from '@pages/pages';
 import './app.pcss';
 
 export const App: React.FC = () => {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      console.log('SW ready');
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => {
+          console.log('SW registered');
+        })
+        .catch((err) => {
+          console.log('SW fail', err);
+        });
+    }
+  }, []);
+
   return (
     <ErrorBoundry>
       <BrowserRouter>
