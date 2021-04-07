cce96807778d8538e722c524c3765643ca4614a9
Andrei Marchenko <marchenko_ad@mail.ru>
2021-04-07 14:54:44 +0300

    update

--- src/sw.js
+++ src/sw.js
@@ -1,16 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-undef */
 
-const CACHE_NAME = 'cache-v2';
-const DYNAMIC_CACHE_NAME = 'dynamic-cache-v2';
+const CACHE_NAME = 'cache-v1';
+const DYNAMIC_CACHE_NAME = 'dynamic-cache-v1';
 
-const URLS = ['/index.html', '/main.css', '/offline.html', '/boundle.js'];
-// const URLS = ['/', '/index.html', '/index.tsx', '/offline.html'];
+const URLS = ['/', './index.html', './main.css', './offline.html', './bundle.js'];
 
 self.addEventListener('install', async (event) => {
   const cache = await caches.open(CACHE_NAME);
 
   await cache.addAll(URLS);
+
+  console.log('Service worker has been installed');
 });
 
 self.addEventListener('activate', async (event) => {
@@ -22,42 +23,27 @@ self.addEventListener('activate', async (event) => {
       .filter((name) => name !== DYNAMIC_CACHE_NAME)
       .map((name) => caches.delete(name))
   );
+
+  console.log('Service worker has been activated');
 });
 
 self.addEventListener('fetch', (event) => {
-  const { request } = event;
-
-  const url = new URL(request.url);
-
-  if (url.origin === location.origin) {
-    event.respondWith(cacheFirst(request));
-  } else {
-    event.respondWith(networkFirst(request));
-  }
+  console.log(`Trying to fetch ${event.request.url}`);
+  event.respondWith(checkCache(event.request));
 });
 
-async function cacheFirst(request) {
-  const cached = await caches.match(request);
-
-  return cached ?? (await fetch(request));
+async function checkCache(req) {
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || checkOnline(req);
 }
 
-async function networkFirst(request) {
+async function checkOnline(req) {
   const cache = await caches.open(DYNAMIC_CACHE_NAME);
-
   try {
-    const response = await fetch(request);
-
-    if (!response || response.status !== 200 || response.type !== 'basic') {
-      return response;
-    }
-
-    await cache.put(request, response.clone());
-
-    return response;
-  } catch (e) {
-    const cached = await cache.match(request);
-
-    return cached ?? (await caches.match('/offline.html'));
+    const res = await fetch(req);
+    await cache.put(req, res.clone());
+    return res;
+  } catch (error) {
+    return caches.match('./offline.html');
   }
 }
