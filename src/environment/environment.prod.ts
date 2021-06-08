54cd47444bbf4b617135a62a7f3477491f80b3e1
Kolya Evtikhovich <evklaus@gmail.com>
2021-06-08 12:23:59 +0300

    fix docker compose

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   praktikum: 'https://ya-praktikum.tech/api/v2',
-  tankUri: 'https://local.ya-praktikum.tech:8080/',
+  tankUri: 'https://neapol-tanks-4.ya-praktikum.tech:8080',
   redirectUri: 'https://tank-batallion-1980.herokuapp.com/login'
 };
