18c79ac1070aa2c1ee9578f66e3fa125935d7f68
Kolya Evtikhovich <evklaus@gmail.com>
2021-06-09 14:29:13 +0300

    fix api conf

--- api/server.ts
+++ api/server.ts
@@ -11,6 +11,19 @@ import routes from './src/routes';
 const app = express();
 const isDevelopment = process.env.NODE_ENV === 'development';
 
+const getCertificates = (isDevelopment: boolean): { key: Buffer; cert: Buffer } => {
+  if (isDevelopment) {
+    return {
+      key: fs.readFileSync(__dirname + '/src/certificates/local.ya-praktikum.tech.pem'),
+      cert: fs.readFileSync(__dirname + '/src/certificates/local.ya-praktikum.tech-key.pem')
+    };
+  }
+  return {
+    key: fs.readFileSync(__dirname + '/src/certificates/privkey.pem'),
+    cert: fs.readFileSync(__dirname + '/src/certificates/fullchain.pem')
+  };
+};
+
 app.use(
   cors({
     credentials: true,
@@ -26,10 +39,9 @@ app.use('/api', routes);
 
 const port = process.env.PORT || 8080;
 
-const certificateIsExist =
-  !isDevelopment &&
-  fs.existsSync(__dirname + '/src/certificates/privkey.pem') &&
-  fs.existsSync(__dirname + '/src/certificates/fullchain.pem');
+const certificates = getCertificates(isDevelopment);
+
+const isCertExist = certificates.cert && certificates.key;
 
 (async () => {
   await sequelize.sync({ force: true });
@@ -37,19 +49,11 @@ const certificateIsExist =
   await Theme.create({ name: 'dark' } as Theme);
   await Theme.create({ name: 'light' } as Theme);
 
-  if (certificateIsExist) {
-    https
-      .createServer(
-        {
-          key: fs.readFileSync(__dirname + '/src/certificates/privkey.pem'),
-          cert: fs.readFileSync(__dirname + '/src/certificates/fullchain.pem')
-        },
-        app
-      )
-      .listen(port, () => {
-        // eslint-disable-next-line no-console
-        console.log('Application is started on localhost HTTPS:', port);
-      });
+  if (isCertExist) {
+    https.createServer(certificates, app).listen(port, () => {
+      // eslint-disable-next-line no-console
+      console.log('Application is started on localhost HTTPS:', port);
+    });
   } else {
     app.listen(port, () => {
       // eslint-disable-next-line no-console
--- api/src/sequelize.ts
+++ api/src/sequelize.ts
@@ -4,9 +4,11 @@ import dbConfig from './config/db.config';
 import { Theme } from './models/theme';
 import { UserTheme } from './models/user_theme';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   port: dbConfig.PORT,
-  host: dbConfig.HOST,
+  host: isDev ? 'localhost' : dbConfig.HOST,
   dialect: dbConfig.dialect as Dialect,
   models: [UserTheme, Theme]
 });
