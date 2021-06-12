f2d3b12385572925abaf1f0fe4a526a241895c6c
Kolya Evtikhovich <evklaus@gmail.com>
2021-06-12 15:27:17 +0300

    fix env

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   praktikum: 'https://ya-praktikum.tech/api/v2',
-  tankUri: 'https://neapol-tanks-4.ya-praktikum.tech:8080/api',
+  tankUri: 'https://neapol-tanks-4.ya-praktikum.tech/api',
   redirectUri: 'https://tank-batallion-1980.herokuapp.com/login'
 };
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   praktikum: 'https://ya-praktikum.tech/api/v2',
-  tankUri: 'https://local.ya-praktikum.tech:8080/api',
+  tankUri: 'https://neapol-tanks-4.ya-praktikum.tech/api',
   redirectUri: 'http://localhost:3000'
 };
